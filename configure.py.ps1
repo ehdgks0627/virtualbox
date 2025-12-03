5cec63336bcd105406bba6d93877182aedc8be73
Andreas Loeffler <andreas.loeffler@oracle.com>
2025-12-03 15:01:49 +0000

    configure.py.ps1: Added initial version (thin PowerShell wrapper for invoking configure.py). bugref:11003
    svn:sync-xref-src-repo-rev: r171665

---
+++ configure.py.ps1
@@ -0,0 +1,46 @@
+# $Id: configure.py.ps1 111996 2025-12-03 15:01:49Z andreas.loeffler@oracle.com $
+# Thin PowerShell wrapper to call configure.py with passed arguments.
+#
+# Copyright (C) 2025 Oracle and/or its affiliates.
+#
+# This file is part of VirtualBox base platform packages, as
+# available from https://www.virtualbox.org.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, in version 3 of the
+# License.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <https://www.gnu.org/licenses>.
+#
+# SPDX-License-Identifier: GPL-3.0-only
+#
+
+$python = Get-Command python3 -ErrorAction SilentlyContinue
+if (-not $python) 
+{
+    $python = Get-Command python -ErrorAction SilentlyContinue
+}
+
+if (-not $python)
+ {
+    Write-Host "Python 3 is required in order to build VirtualBox." -ForegroundColor Red
+    Write-Host "Please install Python 3 and ensure it is in your PATH." -ForegroundColor Red
+    exit 1
+}
+
+$version = & $python.Path --version 2>&1
+if ($version -notmatch "Python 3")
+{
+    Write-Host "Python 3 is required. Found: $version" -ForegroundColor Red
+    exit 1
+}
+
+& $python.Path configure.py @args
+exit $LASTEXITCODE
