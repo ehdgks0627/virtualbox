c21c8d30596789b4a50621afa5e1565728421f93
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2025-12-10 09:24:07 +0000

    IPRT/testcase/Makefile.kmk: Build fix for tstTimer.
    svn:sync-xref-src-repo-rev: r171764

--- src/VBox/Runtime/testcase/tstRTTime.cpp
+++ src/VBox/Runtime/testcase/tstRTTime.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstRTTime.cpp 110684 2025-08-11 17:18:47Z klaus.espenlaub@oracle.com $ */
+/* $Id: tstRTTime.cpp 112092 2025-12-10 09:24:07Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT Testcase - Simple RTTime tests (requires GIP).
  */
@@ -135,7 +135,7 @@ int main()
     }
 
     /* Report debug details: */
-#if defined(RT_ARCH_AMD64) || defined(RT_ARCH_X86) /** @todo This isn't really x86 or AMD64 specific... */
+#ifdef MAY_USE_GIP
     RTTestValue(hTest, "RTTimeDbgSteps",        RTTimeDbgSteps(),                           RTTESTUNIT_OCCURRENCES);
     RTTestValue(hTest, "RTTimeDbgSteps pp",     ((uint64_t)RTTimeDbgSteps() * 1000) / i,    RTTESTUNIT_PP1K);
     RTTestValue(hTest, "RTTimeDbgExpired",      RTTimeDbgExpired(),                         RTTESTUNIT_OCCURRENCES);
