b9baae28bbac9e8d2257b8b7ef2134660320606a
Sergey Dubov <sergey.dubov@oracle.com>
2026-01-09 11:44:52 +0000

    Forward-ported r172094 from 7.1: FE/Qt: Runtime UI: Delaying VM startup modal dialog being shown for customer's build; This is necessary to avoid focus being stolen from guest-screen on i3 WM.
    svn:sync-xref-src-repo-rev: r172096

--- src/VBox/Frontends/VirtualBox/src/runtime/UISession.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UISession.cpp
@@ -1,4 +1,4 @@
-/* $Id: UISession.cpp 111900 2025-11-26 21:43:46Z knut.osmundsen@oracle.com $ */
+/* $Id: UISession.cpp 112391 2026-01-09 11:44:52Z sergey.dubov@oracle.com $ */
 /** @file
  * VBox Qt GUI - UISession class implementation.
  */
@@ -248,16 +248,20 @@ bool UISession::powerUp()
     /* Show "Starting/Restoring" progress dialog: */
     if (isSaved())
     {
+#ifndef VBOX_GUI_WITH_CUSTOMIZATIONS1
         msgCenter().showModalProgressDialog(comProgress, machineName(), ":/progress_state_restore_90px.png", 0, 0);
+#else
+        msgCenter().showModalProgressDialog(comProgress, machineName(), ":/progress_state_restore_90px.png", 0, 10000);
+#endif
         /* After restoring from 'saved' state, machine-window(s) geometry should be adjusted: */
         machineLogic()->adjustMachineWindowsGeometry();
     }
     else
     {
-#ifdef VBOX_IS_QT6_OR_LATER /** @todo why is this any problem on qt6? */
+#ifndef VBOX_GUI_WITH_CUSTOMIZATIONS1
         msgCenter().showModalProgressDialog(comProgress, machineName(), ":/progress_start_90px.png", 0, 0);
 #else
-        msgCenter().showModalProgressDialog(comProgress, machineName(), ":/progress_start_90px.png");
+        msgCenter().showModalProgressDialog(comProgress, machineName(), ":/progress_start_90px.png", 0, 10000);
 #endif
         /* After VM start, machine-window(s) size-hint(s) should be sent: */
         machineLogic()->sendMachineWindowsSizeHints();
