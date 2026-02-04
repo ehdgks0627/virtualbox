5142f72ed4d033dc2dc74ce3602af6a47ea22229
Valery Portnyagin <valery.portnyagin@oracle.com>
2026-02-04 15:24:17 +0000

    jiraref: VBP-1999. Windows11 unattended installation has been activated. ISO image is Win11_25H2_English_x64.iso. Will see how it's going on
    svn:sync-xref-src-repo-rev: r172553

--- src/VBox/ValidationKit/tests/installation/tdGuestOsUnattendedInst1.py
+++ src/VBox/ValidationKit/tests/installation/tdGuestOsUnattendedInst1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# $Id: tdGuestOsUnattendedInst1.py 112690 2026-01-26 10:54:16Z valery.portnyagin@oracle.com $
+# $Id: tdGuestOsUnattendedInst1.py 112823 2026-02-04 15:24:17Z valery.portnyagin@oracle.com $
 
 """
 VirtualBox Validation Kit - Guest OS unattended installation tests.
@@ -37,7 +37,7 @@ terms and conditions of either the GPL or the CDDL or both.
 
 SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
 """
-__version__ = "$Revision: 112690 $"
+__version__ = "$Revision: 112823 $"
 
 
 # Standard Python imports.
@@ -558,7 +558,7 @@ class tdGuestOsInstTest1(vbox.TestDriver):
             UnattendedVm(oSet, 'tst-w10-1903-32', 'Windows10',       '6.0/uaisos/en_windows_10_business_editions_version_1903_x86_dvd_ca4f0f49.iso',                    UnattendedVm.kfWinGaTimesync), # >=7GiB
             UnattendedVm(oSet, 'tst-w10-1903-64', 'Windows10_64',    '6.0/uaisos/en_windows_10_business_editions_version_1903_x64_dvd_37200948.iso',                    UnattendedVm.kfWinGaTimesync), # >=10GiB
             ## @todo r=bird: Add UnattendedVm.kfMinTwoCpus or similar; get & use the msdn iso (e.g. en-us_windows_11_business_editions_version_25h2_updated_jan_2026_x64_dvd_09c1e011.iso) so we know what we're testing
-            #UnattendedVm(oSet, 'tst-w11-1905-64', 'Windows11_64',    '7.1/uaisos/Win11_25H2_English_x64.iso',                                                           UnattendedVm.kfWinGaTimesync), # >=10GiB
+            UnattendedVm(oSet, 'tst-w11-1905-64', 'Windows11_64',    '7.1/uaisos/Win11_25H2_English_x64.iso',                                                           UnattendedVm.kfWinGaTimesync), # >=10GiB
             #
             # Ubuntu
             #
